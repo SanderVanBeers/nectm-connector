e3d840a9cc0b0a7dc8006c4dcd66510dc99af92d
Sander Van Beers <sander.vanbeers@crosslang.com>
2020-07-09 10:55:05 +0200

    add flask_restful

--- connector.py
+++ connector.py
@@ -1,13 +1,33 @@
 import flask
 from requests import post, get
 from flask import request, jsonify
-ADDRESS = 'http://localhost:27979'
+HOST = 'http://localhost:27979'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-access_token = post(f"{ADDRESS}/api/v1/auth", json={'username':'admin','password':'admin'}).json()['access_token']
+access_token = post(f"{HOST}/api/v1/auth", json={'username': USERNAME,'password': PASSWORD}).json()['access_token']
 
 @app.route('/get', methods=['GET'])
+
+class match(self):
+    def __init__:
+        self.id =
+        self.segment =          #result['tu']['source_text']
+        self.translation =      #result['tu']['target_text']
+        self.quality =          #?
+        self.reference =        #result_data['query']
+        self.subject =          #~tag?
+        self.match =            #result['match']
+        self.usage-count =
+        self.created-by = 
+        self.last-updated-by = 
+        self.create-date =
+        self.last-update-date = 
+    def get(self):
+        pass
 
 def get_fuzzy_match():
     data = request.get_json()
@@ -19,7 +39,7 @@ def get_fuzzy_match():
     else:
         slang = langpair.split('-')[0]
         tlang = langpair.split('-')[1]
-    result = get(f"{ADDRESS}/api/v1/tm", headers={"Authorization": f"JWT {access_token}", "Content-Type":"application/json"}, json={"q":q, "slang": slang, "tlang": tlang, "concordance": "true"})
+    result = get(f"{HOST}/api/v1/tm", headers={"Authorization": f"JWT {access_token}", "Content-Type":"application/json"}, json={"q":q, "slang": slang, "tlang": tlang, "concordance": "true"})
     result_data = result.json()
     return result_data
 
