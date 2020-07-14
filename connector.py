1ed9310a92f73a2dd553dae9f6622404e6e09e6f
Sander Van Beers <sander.vanbeers@crosslang.com>
2020-07-14 16:47:09 +0200

    add match object

--- connector.py
+++ connector.py
@@ -11,7 +11,7 @@ app = flask.Flask(__name__)
 
 #access_token = post(f"{HOST}/api/v1/auth", json={'username': USERNAME,'password': PASSWORD}).json()['access_token']
 
-class MatecatReponse():
+class MatecatReponse:
     def __init__(self, responseData={'translatedText': 'dit is een test', 'match': 1}, quotaFinished=False, mtLangSupported=None, responseDetails="" , responseStatus= 200, responderId='235', matches = [], exception_code=None):
         self.responseData = responseData
         self.quotaFinished = quotaFinished
@@ -20,12 +20,31 @@ class MatecatReponse():
         self.responseStatus = responseStatus
         self.responderId = responderId
         self.matches = matches
-
     #method to convert the snake_case of instance variables to json standard of kebab-case expected by MateCat (kebab-case raises assign errors in Python)        
     def getDict(self):      
         dictionary = self.__dict__
         return dict([(dasherize(k), v)
-                     for (k, v) in dictionary.items()])   
+                     for (k, v) in dictionary.items()])
+
+class Match:
+    def __init__(self, id, segment, translation, match, quality='75', reference='dit is een test', subject='All', usage_count=0, created_by='MateCat', last_updated_by='MateCat', create_date='Tue Jul 07 11:47:11 GMT 2020', last_update_date='Tue Jul 07 11:47:11 GMT 2020'):
+        self.id = id
+        self.segment = segment
+        self.translation = translation
+        self.quality = quality
+        self.reference = reference
+        self.subject = subject
+        self.match = match
+        self.usage_count = usage_count
+        self.created_by = created_by
+        self.last_updated_by = last_updated_by
+        self.create_date = create_date
+        self.last_update_date = last_update_date
+    #method to convert the snake_case of instance variables to json standard of kebab-case expected by MateCat (kebab-case raises assign errors in Python)        
+    def getDict(self):      
+        dictionary = self.__dict__
+        return dict([(dasherize(k), v)
+                     for (k, v) in dictionary.items()])
 
 class TmView(MethodView):
     def get(self):
@@ -46,20 +65,7 @@ class TmView(MethodView):
             results = result_data['results']
             if not results[0]['tu']['source_text'] == " ":
                 for result in results:
-                    match = {
-                        'id' : str(results.index(result)),
-                        'segment' : result['tu']['source_text'],
-                        'translation' : result['tu']['target_text'],
-                        'quality': "75",
-                        'reference' : 'dit is een test',
-                        'subject' : 'All',
-                        'match' : float(result['match']/100),
-                        'usage-count' : 0,
-                        'created-by' : 'MateCat',
-                        'last-updated-by' : 'MateCat',
-                        'create-date': 'Tue Jul 07 11:47:11 GMT 2020',
-                        'last-update-date' : 'Tue Jul 07 11:47:11 GMT 2020',
-                    }
+                    match = Match(id=str(results.index(result)), segment=result['tu']['source_text'], translation=result['tu']['target_text'], match=float(result['match']/100)).getDict()
                     matches.append(match)
         return_blob = MatecatReponse(matches = matches)
         return return_blob.getDict()
